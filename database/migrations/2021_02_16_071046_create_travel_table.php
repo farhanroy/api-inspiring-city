c8f4fc27ee97226c6fec91679e6ad60995e1288b
farhanroy <farhanroy120@gmail.com>
2021-02-16 14:28:14 +0700

    adding table travel

---
+++ database/migrations/2021_02_16_071046_create_travel_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string('caption');
+            $table->text('description');
+            $table->string('thumnail');
+            $table->string('address');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('image3');
+            $table->string('image4');
+            $table->string('image5');
+            $table->string('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+}
